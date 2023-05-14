a620dd0f7db49dc3d1ca73825ff361b3ab4746de
Vadim Kostenko <kostenko@mdrk.ru>
2023-05-14 17:15:05 +0700

    15pr

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const { errors } = require('celebrate');
 const router = require('./routes');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 app.use(express.json());
@@ -11,7 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 // автотесты не дают записать тут записать из env, на локальной машине работает
 
+app.use(requestLogger);
 app.use(router);
+app.use(errorLogger);
+
 app.use(errors());
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
